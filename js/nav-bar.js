fe18dffab9e2abfef6e9ecbd2124ba1535d0fc2f
neider28 <neidersilva2802@gmail.com>
2022-05-09 18:05:02 -0500

    Nuevos cambios en estilos

---
+++ js/nav-bar.js
@@ -0,0 +1,9 @@
+function openSM() {
+    document.getElementById("mySidemenu").style.width = "280px";
+    document.getElementById("pg-content").style.marginLeft = "10";
+}
+
+function closeSM() {
+    document.getElementById("mySidemenu").style.width = "0";
+    document.getElementById("pg-content").style.marginLeft = "10";
+}
